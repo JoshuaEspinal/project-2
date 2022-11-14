8164271e73052ccd4ef2ed379449d10e911f4949
KuuraKurta <joshuaespinal9401@gmail.com>
2022-11-14 17:08:40 -0500

    Added review models and seeds

--- seeds/index.js
+++ seeds/index.js
@@ -1,14 +1,22 @@
 const sequelize = require("../config/connection");
-const User = require("../models/User");
+const { User, Review } = require("../models");
 const userSeeds = require("./user-seeds.json");
+const reviewSeeds = require("./review-seeds.json");
 
 const seedUsers = async () => {
   await sequelize.sync({ force: true });
 
-  await User.bulkCreate(userSeeds, {
+  const users = await User.bulkCreate(userSeeds, {
     individualHooks: true,
     returning: true,
   });
+
+  for (let review of reviewSeeds) {
+    await Review.create({
+      ...review,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+  }
 
   process.exit(0);
 };
