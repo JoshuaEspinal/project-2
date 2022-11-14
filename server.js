d27fd7cb7c5a537311271e02c78607ac3b2c8e19
KuuraKurta <joshuaespinal9401@gmail.com>
2022-11-14 10:42:02 -0500

    Added login route

--- server.js
+++ server.js
@@ -1,12 +1,28 @@
 const express = require("express");
+const session = require("express-session");
 const sequelize = require("./config/connection");
-console.log(sequelize);
+const routes = require("./routes");
+
 const app = express();
 const port = process.env.PORT || 3000;
+
+const sess = {
+  secret: process.env.SESSION_SECRET,
+  cookie: {},
+  resave: false,
+  saveUninitialize: true,
+};
+
+app.use(session(sess));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(port, () => {
